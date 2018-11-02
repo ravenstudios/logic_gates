f8942eff297053d1f3e847f4f96189df6d707ccf
ravenstudios <ravenstudiosrob@yahoo.com>
2018-11-01 23:27:54 -0500

    added binary converters

--- js.js
+++ js.js
@@ -66,3 +66,39 @@ function norGate(input1, input2){
 function xnorGate(input1, input2){
   return notGate(xorGate(input1, input2))
 }
+
+
+function decToBinary(dec){
+  if(dec <= 255 && Number.isInteger(dec)){
+    var temp;
+    var binArray = [128, 64, 32, 16, 8, 4, 2, 1];
+    var resultArray = [];
+    for (var i = 0; i < binArray.length; i++) {
+      if(dec >= binArray[i]){
+        resultArray.push(1);
+        dec = dec - binArray[i]
+      }
+      else{
+        resultArray.push(0);
+      }
+    }
+    return resultArray;
+  }
+  else{
+    alert("Must use numbers and number must be below 256");
+    return[0, 0, 0, 0, 0, 0, 0, 0];
+  }
+}
+
+function binaryToDec(arr) {
+  var binArray = [128, 64, 32, 16, 8, 4, 2, 1];
+  var result = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i] === 1){
+      result += binArray[i];
+    }
+  }
+  return result;
+}
+console.log(binaryToDec([1, 1, 1, 1, 1, 1, 1, 1]));
+console.log(decToBinary(255));
