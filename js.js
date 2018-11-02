ce3bf93b73aaedd34fe58a6a0cf0f8b85e9805cd
ravenstudios <ravenstudiosrob@yahoo.com>
2018-11-02 00:07:17 -0500

    added a function that takes 2 arrays and adds them and retuens an array of the result

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 $(()=>{
-  console.log("jq ready");
+  //console.log("jq ready");
 
   $(".input").click((e)=>{
     console.log(e.target.id);
@@ -14,7 +14,7 @@ $(()=>{
 // console.log("C: 1, 0 : " + halfAdder(1, 0).c);
 // console.log("S: 1, 1 : " + halfAdder(1, 1).s);
 // console.log("C: 1, 1 : " + halfAdder(1, 1).c);
-console.log(fullAdder(1,1, 1));
+// console.log(fullAdder(1,1, 1));
 
 // console.log("0 : " + notGate(0));
 // console.log("1 : " + notGate(1));
@@ -79,11 +79,11 @@ function halfAdder(input1, input2){
 }
 
 function fullAdder(input1, input2, carry){
-  // var halfA = halfAdder(input1, input2);
-  // var halfB = halfAdder(halfA.s, halfA.c);
-  // var s = halfB.s;
-  // var  c = or(halfA.c, halfB.c);
-  // return {s: s, c: c};
+
+
+  if(!carry){
+    carry = 0;
+  }
 
   var xor1 = xor(input1, input2);
   var xor2 = xor(xor1, carry)
@@ -96,6 +96,7 @@ function fullAdder(input1, input2, carry){
 
 
 function decToBinary(dec){
+  console.log("dec: " + dec);
   if(dec <= 255 && Number.isInteger(dec)){
     var temp;
     var binArray = [128, 64, 32, 16, 8, 4, 2, 1];
@@ -127,5 +128,40 @@ function binaryToDec(arr) {
   }
   return result;
 }
-console.log(binaryToDec([1, 1, 1, 1, 1, 1, 1, 1]));
-console.log(decToBinary(255));
+// console.log(binaryToDec([1, 1, 1, 1, 1, 1, 1, 1]));
+// console.log(decToBinary(255));
+
+
+function add(arr1, arr2) {
+
+// console.log(arr1[0]);
+  var result = [0, 0, 0, 0, 0, 0, 0];
+  var start = halfAdder(arr1[7], arr2[7]);
+
+  result[7] = start.s;//SUM
+  var c = start.c; //CARRY
+console.log(result);
+
+  for (var i = 6; i >= 0; i--) {
+    var a = arr1[i];
+    var b = arr2[i];
+
+    var temp = fullAdder(a, b, c);
+    console.log(temp);
+    result[i] = temp.s;
+    c = temp.c;
+  }
+
+  if(c === 1){
+    alert("Overflow");
+    return [0, 0, 0, 0, 0, 0, 0, 0];
+  }
+  else{
+    return result;
+  }
+
+}
+
+var r = add(decToBinary(6), decToBinary(250));
+console.log(r);
+console.log(binaryToDec(r));
